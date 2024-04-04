a11181f000ddf9b9b839818078a42724932ed0f1
David Liebert <david.liebert@kasna.com.au>
2024-04-05 10:02:00 +1300

    slight reorg

--- servers/grpc/grpc.go
+++ servers/grpc/grpc.go
@@ -12,34 +12,34 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-// grpcServer implements the Frontend interface and the ApiServer interface (from proto)
+// grpcServer implements the Frontend and the ApiServer interfaces
 type grpcServer struct {
 	proto.UnimplementedApiServer
-	backend interfaces.Backend
-	addr    string
-	port    int
+	interfaces.Backend
+	addr string
+	port int
 }
 
 func NewGrpcServer(ctx context.Context, addr string, port int, backend interfaces.Backend) (grpcServer, error) {
-	return grpcServer{backend: backend, addr: addr, port: port}, nil
+	return grpcServer{Backend: backend, addr: addr, port: port}, nil
 }
 
-func (a grpcServer) GetUsers(ctx context.Context, _ *emptypb.Empty) (*proto.GetUsersResponse, error) {
-	users := a.backend.GetAllUsers()
+func (g grpcServer) GetUsers(ctx context.Context, _ *emptypb.Empty) (*proto.GetUsersResponse, error) {
+	users := g.Backend.GetAllUsers()
 	return &proto.GetUsersResponse{Message: fmt.Sprintf("%v", users)}, nil
 }
 
-func (a grpcServer) GetUser(ctx context.Context, u *proto.UserId) (*proto.GetUserResponse, error) {
+func (g grpcServer) GetUser(ctx context.Context, u *proto.UserId) (*proto.GetUserResponse, error) {
 	id := u.Message
-	user, _ := a.backend.GetUserById(int(id))
+	user, _ := g.Backend.GetUserById(int(id))
 	return &proto.GetUserResponse{Message: fmt.Sprintf("%v", user)}, nil
 }
 
-func (a grpcServer) Run() error {
+func (g grpcServer) Run() error {
 	gsrv := grpc.NewServer()
-	proto.RegisterApiServer(gsrv, a)
+	proto.RegisterApiServer(gsrv, g)
 	reflection.Register(gsrv)
-	lis, _ := net.Listen("tcp", fmt.Sprintf("%s:%d", a.addr, a.port))
+	lis, _ := net.Listen("tcp", fmt.Sprintf("%s:%d", g.addr, g.port))
 	gsrv.Serve(lis)
 	return nil
 }
